3c060afa0adbb0fb8b43c4b59082dc736c82ae10
treichert <treichert@WSSH004_07212.fsbwit.intern>
2015-06-10 13:28:51 +0200

    CtrlServiceContent dabei

---
+++ src/generators/CtrlServiceContent.java
@@ -0,0 +1,40 @@
+package generators;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class CtrlServiceContent implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		
+		if (event.getActionCommand().equals("Neuer Kunde")) {
+			JOptionPane.showMessageDialog(null,
+				    "This function is under construction!",
+				    "Meldung",
+				    JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if (event.getActionCommand().equals("Kunde editieren")) {
+			JOptionPane.showMessageDialog(null,
+				    "This function is under construction!",
+				    "Meldung",
+				    JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if (event.getActionCommand().equals("Kunde löschen")) {
+			JOptionPane.showMessageDialog(null,
+				    "This function is under construction!",
+				    "Meldung",
+				    JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if (event.getActionCommand().equals("Kunde suchen")) {
+			JOptionPane.showMessageDialog(null,
+				    "This function is under construction!",
+				    "Meldung",
+				    JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+	}
+	
+}
